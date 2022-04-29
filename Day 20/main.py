8fcc041dec48ae392a95adc68ad44aabd067300b
Tom Rook <93784825+TomRook12@users.noreply.github.com>
2022-04-29 21:19:21 +0100

    End of day

--- Day 20/main.py
+++ Day 20/main.py
@@ -1,5 +1,6 @@
 from turtle import Screen
 from snake import Snake
+from food import Food
 import time
 
 screen = Screen()
@@ -10,6 +11,7 @@ snakes = []
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
 
 screen.listen()
 screen.onkey(key="Up", fun=snake.up)
@@ -22,8 +24,11 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
+
+    #Detect collision
+    if snake.head.distance(food) <15:
+        food.refresh()
 
 screen.exitonclick()
 
